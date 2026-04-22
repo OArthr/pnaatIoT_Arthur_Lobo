cd090efdd5b9e58ddf45af5c7f209213d48dbdb2
OArthr <130790176+OArthr@users.noreply.github.com>
2026-04-21 23:03:34 -0300

    Remove test break statement in main loop
    Removed test break statement from the loop.

--- src/main.py
+++ src/main.py
@@ -232,5 +232,4 @@ sistema = SistemaSemaforo(semaforos, sensores, botao, ModosConfig)
 while True:
   sistema.loop()
   print(sistema.tempo_ativo)
-  break # Testando se o actions aceita, REMOVER AO TESTAR
   time.sleep(1)
